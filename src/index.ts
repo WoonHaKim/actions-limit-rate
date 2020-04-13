403e27d5c1205cbb7e3d10cd69076e5571f1074f
Unna Kim <work@unnakim.info>
2020-04-13 12:47:36 +0900

    init with code

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import core from "@actions/core";
+import github from "@actions/github";
+
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const rate = core.getInput("rate");
+    const repoToken = core.getInput("repo-token");
+
+    console.log(`rate set to ${rate}`);
+
+    const githubClient = new github.GitHub(repoToken);
+
+    const {
+      data: workflowLists,
+    } = await githubClient.actions.listRepoWorkflows(github.context.repo);
+    const workingWorkflow = workflowLists.workflows.find(
+      (w) => w.name === github.context.workflow
+    );
+
+    const {
+      data: workflowHistory,
+    } = await githubClient.actions.listWorkflowRuns({
+      ...github.context.repo,
+      workflow_id: workingWorkflow?.id!,
+    });
+
+    const lastSuccessWorkflow = workflowHistory.workflow_runs.find(
+      (ww) => ww.status === "success"
+    );
+
+    const lastSuccessWorkflowDate = new Date(
+      lastSuccessWorkflow?.created_at!
+    ).getTime();
+    const interval = Math.floor(
+      new Date().getTime() - lastSuccessWorkflowDate / 1000
+    );
+
+    if (interval < 600)
+      await githubClient.actions.cancelWorkflowRun({
+        ...github.context.repo,
+        run_id: (github.context as any).run_id,
+      });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
